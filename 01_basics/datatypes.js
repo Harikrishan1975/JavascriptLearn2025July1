f381ccb2bf6fb36349ccf26b9b08f42e31396275
Harikrishan1975 <143389485+Harikrishan1975@users.noreply.github.com>
2025-07-01 06:44:55 +0000

    datatype summery

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,37 @@
+// primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+  
+
+// const score = 100
+// const scoreValue = 100.3
+
+
+// const isLoggedIn = false
+
+// const outsideTmep = null
+
+// let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+
+
+// Referece (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman ", "naagraj", "doga"];
+let myObj= {
+    name: "pankaj",
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+
+console.log(typeof myObj);
---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+const accountId =   133445
+
+let accountEmail = "pankaj@google.com"
+var accountPassword = "1334423"
+accountCity = "Shimla"
+console.log()
+
+// accountId = 2.  //not allowed 
+
+accountEmail = "hc@hc.com"
+
+accountPassword = "13133"
+
+accountCity = "Jaipur"
+
+
+console.table([accountEmail, accountEmail , accountId, accountCity])
+
+
+console.log(accountId)
