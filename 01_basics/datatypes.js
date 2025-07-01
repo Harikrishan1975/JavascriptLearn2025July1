f877a6b9f9b199aac4f09fd28223a31b6961a52f
Harikrishan1975 <143389485+Harikrishan1975@users.noreply.github.com>
2025-07-01 08:06:23 +0000

    exampleof nums and math

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,37 +1,64 @@
-// primitive 
+// // // primitive 
 
-// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+// // // 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
   
 
-// const score = 100
-// const scoreValue = 100.3
+// // // const score = 100
+// // // const scoreValue = 100.3
 
 
-// const isLoggedIn = false
+// // // const isLoggedIn = false
 
-// const outsideTmep = null
+// // // const outsideTmep = null
 
-// let userEmail;
+// // // let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id === anotherId);
-
-
-
-// Referece (Non Primitive)
-
-// Array, Objects, Functions
-
-const heros = ["shaktiman ", "naagraj", "doga"];
-let myObj= {
-    name: "pankaj",
-    age: 23,
-}
-
-const myFunction = function(){
-    console.log("Hello world");
-}
+// // const id = Symbol('123')
+// // const anotherId = Symbol('123')
+// // console.log(id === anotherId);
 
 
-console.log(typeof myObj);
+
+// // // Referece (Non Primitive)
+
+// // // Array, Objects, Functions
+
+// // const heros = ["shaktiman ", "naagraj", "doga"];
+// // let myObj= {
+// //     name: "pankaj",
+// //     age: 23,
+// // }
+
+// // const myFunction = function(){
+// //     console.log("Hello world");
+// // }
+
+
+// // console.log(typeof myObj);
+
+// //  Stack (Primitive), Heap(Non-Primitive)// reference original value 
+
+
+// let myYoutubename = "pankaj"
+
+// let anothername = myYoutubename
+// anothername = "chaiaurcode"
+
+// console.log(anothername);
+// console.log(myYoutubename);
+
+
+// let userOne = {
+//     email: "user@google.com",
+//     upi : "user@ybl"
+// }
+
+
+// let userTwo = userOne
+
+// userTwo.email = "hitesh@google.com"
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
+
---
+++ 01_basics/string.js
@@ -0,0 +1,37 @@
+// // const name = "hitesh"
+
+// // const repoCount = 50
+
+// // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+// const gameName = new String('hitesh-hc')
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newStrng = gameName.substring(0,4)
+// console.log(newStrng);
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString)
+
+
+// trim , replace
+
+const newStringOne = "      hitesh.      "
+
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://hitesh,com/"
+
+console.log((url.replace(",","-")))
+
+
+
