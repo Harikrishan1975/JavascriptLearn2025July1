a191cc24718b483dcb80ffa296e6d711ba96dcae
Harikrishan1975 <143389485+Harikrishan1975@users.noreply.github.com>
2025-07-02 05:49:56 +0000

    array and data

---
+++ 01_basics/array.js
@@ -0,0 +1,60 @@
+// // /// array 
+
+// const myArr = [0,1,2,3,4,5]
+// // const myHerors = ["shaktiman", "nagraj"]
+
+// // const myArr2 = new Array(1,2,3,4)
+// // console.log(myArr2[0]);
+
+
+// // // Array Methods
+
+// // myArr.push(6)
+// // myArr.push(7)
+// // myArr.pop()
+
+// // myArr.unshift(0)
+// // myArr.shift()
+// // console.log(myArr);
+
+
+// // console.log(myArr.includes(9));
+// // console.log(myArr.indexOf(9))
+// // 
+// // CONST;
+
+// const newArr = myArr.join()
+
+// console.log(typeof newArr);
+
+// console.log("A", myArr);
+// const myn1 = myArr.slice(1,3)
+
+// const items = ["banana", "apple", "mango", "cherry"]
+// const result = items.splice(0,4)
+// console.log(result);
+// console.log(items);
+
+const another_arrat = [1,2,3,[4,5,6],7,[4,5]]
+
+const real_another_array = another_arrat.flat(Infinity)
+console.log(real_another_array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/date.js
@@ -0,0 +1,31 @@
+let mydate  = new Date()
+
+// console.log(mydate.toString())
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toDateString())
+
+let myCreateDate = new Date("01-14-2023")
+
+// console.log(myCreateDate.toDateString);
+
+let timeStamp = Date.now()
+
+console.log(Math.floor(Date.now()/1000));
+
+let newdate = new Date()
+console.log(newdate);
+console.log(newdate.getMonth());
+
+
+newdate.toLocaleString('default',{
+    weekday : "long",
+})
+
+
+
+
+
+
+
+// console.log(myCreateDate.toLocaleString());
+
